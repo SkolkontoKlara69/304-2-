360114ad5d4c894568caa7c2bbfbcc9e9e5a85df
SkolkontoKlara420 <kevin.backstrom@elev.ga.klaragymnasium.se>
2023-03-13 15:06:00 +0100

    Movement för karaktären
    Fixade movement för karaktären och så att man kan kolla runt

---
+++ 304 -2-/Assets/Scripts/LookAround.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LookAround : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensX;  
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 40f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}

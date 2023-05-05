c7f9cd0d0aa8801cb5512a7cce5f700bcb929dd3
PredatorPrimus <simon@sennero.se>
2023-05-05 11:21:36 +0200

    Revert "Revert "Hp screen""
    This reverts commit 6ee17f889884e72e3c0b8e299e19a81e89e16107.

--- 304 -2-/Assets/Scripts/Player304.cs
+++ 304 -2-/Assets/Scripts/Player304.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player304 : MonoBehaviour
 {
@@ -24,13 +26,37 @@ public class Player304 : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody>();
         position = transform.position;
+<<<<<<< Updated upstream
         meshCollider = GetComponent<MeshCollider>();
         //defaultColliderHeight = meshCollider.height;
+=======
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        defaultColliderHeight = capsuleCollider.height;
+
+        isGrounded = true;
+
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
+=======
+        
+
+        if (isGrounded == true)
+        {
+            rigidbody.drag = groundDrag;
+        }
+        else
+        {
+            rigidbody.drag = 0;
+        }
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+>>>>>>> Stashed changes
         //WASD
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -42,6 +68,45 @@ public class Player304 : MonoBehaviour
         rigidbody.MovePosition(transform.position + movement * Time.deltaTime);
 
 
+<<<<<<< Updated upstream
+=======
+        Vector3 playerDirection = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+
+        moveDirection = orientation.forward.normalized * verticalInput + orientation.right * horizontalInput;
+
+        rigidbody.AddForce(moveDirection.normalized * movingSpeed * 10f, ForceMode.Force);
+
+        if (isGrounded == true)
+        {
+            rigidbody.AddForce(moveDirection.normalized * movingSpeed * 10f, ForceMode.Force);
+        }
+        else if (isGrounded == false)
+        {
+            rigidbody.AddForce(moveDirection.normalized * movingSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+
+        if (Input.GetKey(jumpKey) && readyToJump == true /*&& isGrounded*/)
+        {
+            Jump();
+
+            readyToJump = false;
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+    }
+
+    private void Jump()
+    {
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpForce*Time.deltaTime, rigidbody.velocity.z);
+
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+>>>>>>> Stashed changes
     }
 
     public void TakeDamage(float damage)
